4050172339cdc28125f85abe6465e0841391b008
Arbiter09 <100960442+Arbiter09@users.noreply.github.com>
2025-04-08 00:02:16 -0400

    Added boilerplate code

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.resolve.alias["@components"] = path.join(__dirname, "components");
+    return config;
+  },
+};
 
 export default nextConfig;
